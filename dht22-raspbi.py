eb8477521043d65773b112fd6bbab6b9737f7a79
Point-SimoSiren <45048404+Point-SimoSiren@users.noreply.github.com>
2021-02-18 01:24:44 +0200

    Update dht22-raspbi.py

--- dht22-raspbi.py
+++ dht22-raspbi.py
@@ -6,21 +6,37 @@ sudo apt-get install python3-dev python3-pip
 sudo python3 -m pip install --upgrade pip setuptools wheel
 sudo pip3 install Adafruit_DHT
 '''
-
+import os
+import time
 import Adafruit_DHT
+import math
+import requests
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
+def truncate(number, digits) -> float:
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
+
+print("Starting")
+
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    print("Raw humidity: ", humidity)
+    print("Raw temperature: ", temperature)
+    h = truncate(humidity, 2)
+    t = truncate(temperature, 2)
+    print("Rounded humidity: ", h)
+    print("Rounded temperature: ", t)
 
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-        
-    else:
-        print("Failed to retrieve data from humidity sensor")
-       
- # This code is from https://pimylifeup.com/raspberry-pi-humidity-sensor-dht22/
- 
- # Im soon going to reform it to be able to send data to backend rest api
+    url = 'https://timesheetrest.azurewebsites.net/api/measurements'
+
+    mittaus = {'Sender': 'rpi+DHT22/Simo', 'Temperature': t, 'Humidity': h, 'Pressure': 0 }
+
+    x = requests.post(url, json = mittaus)
+
+    print("Back-end vastasi: ", x.text)
+
+    time.sleep(10)
+
